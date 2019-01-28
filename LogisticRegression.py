d736f7a15318327d195b211591a93e10b3a9600a
nathanrowe4 <nathanrowe@bell.net>
2019-01-28 16:04:14 -0700

    Changed function name to execute_activation

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -4,7 +4,7 @@ import numpy as np;
 def sigmoid(z):
 	return 1 / (1 + np.exp(-z))
 
-def get_activation(w, b, X):
+def execute_activation(w, b, X):
 	return sigmoid(np.dot(w.T,X) + b)
 
 def initialize(dim):
