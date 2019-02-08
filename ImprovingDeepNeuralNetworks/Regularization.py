32a52eb2e57a2b22254494474be5b6c9b6d03b1d
nathanrowe4 <nathanrowe@bell.net>
2019-02-08 10:45:54 -0700

    Add Regularization.py

---
+++ ImprovingDeepNeuralNetworks/Regularization.py
@@ -0,0 +1,102 @@
+import numpy as np
+
+def relu(Z):
+    return max(0, Z)
+
+def sigmoid(Z):
+    return 1 / (1 + np.exp(-Z))
+
+#L2 regularization
+def compute_cost_with_regularization(A3, Y, parameters, lambd):
+    m = Y.shape[1]
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    W3 = parameters["W3"]
+    
+    cross_entropy_cost = compute_cost(A3, Y) # This gives you the cross-entropy part of the cost
+    
+    L2_regularization_cost = (lambd / 2) * (np.sum(np.square(W1)) + np.sum(np.square(W2)) + np.sum(np.square(W3))) / m
+    
+    cost = cross_entropy_cost + L2_regularization_cost
+    
+    return cost
+
+def backward_propagation_with_regularization(X, Y, cache, lambd):
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    dZ3 = A3 - Y
+    
+    dW3 = 1./m * np.dot(dZ3, A2.T) + (lambd / m) * cache[10]
+    db3 = 1./m * np.sum(dZ3, axis=1, keepdims = True)
+    
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1./m * np.dot(dZ2, A1.T) + (lambd / m) * cache[6]
+    db2 = 1./m * np.sum(dZ2, axis=1, keepdims = True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1./m * np.dot(dZ1, X.T) + (lambd / m) * cache[2]
+    db1 = 1./m * np.sum(dZ1, axis=1, keepdims = True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+    
+    return gradients
+
+#dropout
+def forward_propagation_with_dropout(X, parameters, keep_prob = 0.5):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+    
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+    D1 = np.random.rand(A1.shape[0], A1.shape[1]) < keep_prob
+    A1 = np.multiply(A1, D1)
+    A1 /= keep_prob
+
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+    D2 = np.random.rand(A2.shape[0], A2.shape[1]) < keep_prob
+    A2 = np.multiply(A2, D2)
+    A2 /= keep_prob
+
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3)
+    
+    cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+    
+    return A3, cache
+
+def backward_propagation_with_dropout(X, Y, cache, keep_prob):
+    m = X.shape[1]
+    (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    dZ3 = A3 - Y
+    dW3 = 1./m * np.dot(dZ3, A2.T)
+    db3 = 1./m * np.sum(dZ3, axis=1, keepdims = True)
+    dA2 = np.dot(W3.T, dZ3)
+    dA2 = np.multiply(dA2, cache[6])
+    dA2 /= keep_prob
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1./m * np.dot(dZ2, A1.T)
+    db2 = 1./m * np.sum(dZ2, axis=1, keepdims = True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+    dA1 = np.multiply(dA1, cache[1])
+    dA1 /= keep_prob
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1./m * np.dot(dZ1, X.T)
+    db1 = 1./m * np.sum(dZ1, axis=1, keepdims = True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+    
+    return gradients
